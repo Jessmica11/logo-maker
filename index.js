5360467a8962940456acada956ca517033cbac9e
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-12-02 10:14:39 -0500

    added comments for main program file

---
+++ index.js
@@ -0,0 +1,22 @@
+// require libraries/directories here
+
+
+
+// create a function for user input (inquirer)
+// 3-characters for logo
+// text color
+// shape choices as 'list' from inquirer
+// shape color
+
+
+
+
+// return the function
+
+
+
+// will need function to write logo file based on user input
+// should probably add code so that it creates unique files each time
+
+
+run(); 
